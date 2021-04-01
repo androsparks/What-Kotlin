6ff474aa0c513c5eba3d4e0e53578fdc05fbda8e
y1xian <yyxnb@outlook.com>
2021-04-01 18:43:07 +0800

    update

--- common-res/src/main/java/com/yyxnb/common_res/config/BaseAPI.kt
+++ common-res/src/main/java/com/yyxnb/common_res/config/BaseAPI.kt
@@ -1,33 +1,32 @@
-package com.yyxnb.common_res.config;
+package com.yyxnb.common_res.config
 
+import com.yyxnb.common_res.utils.UrlInterceptor
+import com.yyxnb.what.core.NetworkUtils
 
-import com.yyxnb.common_res.utils.UrlInterceptor;
-import com.yyxnb.what.core.NetworkUtils;
+object BaseAPI {
 
-public final class BaseAPI {
-
-
-    // 切换请求url  @Headers()
-
-    // 本地
-    public static final String HEADER_LOCAL = UrlInterceptor.URL_PREFIX + BaseAPI.URL_LOCAL;
-    //apiopen
-    public static final String HEADER_APIOPEN = UrlInterceptor.URL_PREFIX + BaseAPI.URL_APIOPEN;
-    // 玩安卓
-    public static final String HEADER_WAN = UrlInterceptor.URL_PREFIX + BaseAPI.URL_WAN_ANDROID;
-    // 极速
-    public static final String HEADER_JISU = UrlInterceptor.URL_PREFIX + BaseAPI.URL_JISU;
-
-
-    //====\key
-
+    //==== key
     // 本地ip
-    public final static String URL_LOCAL = String.format("http:/%s:8080/", NetworkUtils.getLocalIPAddress());
+    var URL_LOCAL = String.format("http:/%s:8080/", NetworkUtils.getLocalIPAddress())
 
     //免费开放接口API
-    public final static String URL_APIOPEN = "https://api.apiopen.top/";
-    public final static String URL_MOCKY = "https://run.mocky.io/";
-    public final static String URL_WAN_ANDROID = "https://www.wanandroid.com/";
-    public final static String URL_JISU = "https://api.jisuapi.com/";
+    const val URL_APIOPEN = "https://api.apiopen.top/"
+    const val URL_MOCKY = "https://run.mocky.io/"
+    const val URL_WAN_ANDROID = "https://www.wanandroid.com/"
+    const val URL_JISU = "https://api.jisuapi.com/"
+
+    // 切换请求url
+    // 本地
+    var HEADER_LOCAL = UrlInterceptor.URL_PREFIX + URL_LOCAL
+
+    //apiopen
+    const val HEADER_APIOPEN = UrlInterceptor.URL_PREFIX + URL_APIOPEN
+
+    // 玩安卓
+    const val HEADER_WAN = UrlInterceptor.URL_PREFIX + URL_WAN_ANDROID
+
+    // 极速
+    const val HEADER_JISU = UrlInterceptor.URL_PREFIX + URL_JISU
+
 
 }
